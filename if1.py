dcf7bbfcd5059ef5c82a99f13a56336145cd6aab
parksontyc <parkson.tyc.chen@gmail.com>
2022-04-09 19:45:40 +0800

    r

--- if1.py
+++ if1.py
@@ -1,5 +1,13 @@
+# else
+# elif
+# 型別轉換
+
 age = input('請輸入年紀：')
 age = int(age) # 型別轉換
 
-if age == 20:
+if age < 20:
     print('菜鳥哦')
+elif age >= 20 and age < 30:
+    print('有年紀了哦')
+else:
+    print('老人')
