48055ac015f1673d74ad28163d1ffd2e51203237
parksontyc <parkson.tyc.chen@gmail.com>
2022-04-16 01:36:46 +0800

    0415

--- try.py
+++ try.py
@@ -1,3 +1,4 @@
+from cmath import pi
 from operator import index
 
 
@@ -5,4 +6,22 @@ mystring1 = 'Hello'
 # mystring1[0] = 'H'
 # print(mystring1)
 
-print(len(mystring1))
+# print(len(mystring1))
+
+# name = 'Parkson'
+# print(name.upper())
+
+# name = 'Parkson'
+# name = name.upper()
+# print(name)
+
+# myname = 'Parkson Chen'
+# print(list(myname))
+
+# print('i have a dream {}'.format(1))
+# print('{}, {}, {}, {}'.format(1, 'haha', '22', pi))
+# print('{3}, {1}, {2}, {0}'.format(1, 'haha', '22', pi))
+
+myname = 'parkson'
+age = 40
+print(f'my name is {myname}, age is {age}')
