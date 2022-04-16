1f03457a424e19f07bd07d42b8971021490e3ecc
parksontyc <parkson.tyc.chen@gmail.com>
2022-04-17 00:06:13 +0800

    list

--- try.py
+++ try.py
@@ -1,5 +1,6 @@
 from cmath import pi
 from operator import index
+from re import A, X
 
 
 mystring1 = 'Hello'
@@ -22,6 +23,41 @@ mystring1 = 'Hello'
 # print('{}, {}, {}, {}'.format(1, 'haha', '22', pi))
 # print('{3}, {1}, {2}, {0}'.format(1, 'haha', '22', pi))
 
-myname = 'parkson'
-age = 40
-print(f'my name is {myname}, age is {age}')
+# myname = 'parkson'
+# age = 40
+# print(f'my name is {myname}, age is {age}')
+
+# myname = 'parkson'
+# myname = 'P' + myname[1:]
+# print(myname)
+
+# mylist = [1, 4, 9]
+# mylist[0] = 8
+# print(mylist)
+
+# mylist = ['a', 'b', 'c', 'd']
+# mylist.reverse()
+# mylist = mylist[::-1]
+
+# mylist = ['a', '2', 's', 'r']
+# lost_element = mylist.pop()
+# print(f'{mylist}, {lost_element}')
+
+# a = 12
+# b = a
+# b = 15
+# print(a)
+# print(b)
+
+
+# x = [1, 3, 5, 6, 7]
+# y = x
+# y[0] = 12
+# print(x)
+# print(y)
+
+x = [1, 3, 5, 6, 7]
+y = x.copy()
+y[0] = 12
+print(x)
+print(y)
